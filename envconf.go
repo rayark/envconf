76a339825ec0b595726b73c6d440650673fae886
Yao Wei (魏銘廷) <mwei@rayark.com>
2017-07-04 10:22:37 +0800

    feat: add uint parsing

--- envconf.go
+++ envconf.go
@@ -17,12 +17,27 @@ func loadField(name string, out *reflect.Value) {
 	switch out.Kind() {
 	case reflect.String:
 		loadString(name, out)
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		loadInteger(name, out)
+
+	case reflect.Int,
+		reflect.Int8,
+		reflect.Int16,
+		reflect.Int32,
+		reflect.Int64:
+		loadInt(name, out)
+
+	case reflect.Uint,
+		reflect.Uint8,
+		reflect.Uint16,
+		reflect.Uint32,
+		reflect.Uint64:
+		loadUint(name, out)
+
 	case reflect.Bool:
 		loadBool(name, out)
+
 	case reflect.Struct:
 		loadStruct(name, out)
+
 	default:
 		panic(fmt.Errorf("field type of %s cannot be recognized by envconf", name))
 	}
@@ -55,17 +70,31 @@ func loadString(name string, out *reflect.Value) {
 	out.SetString(data)
 }
 
-func loadInteger(name string, out *reflect.Value) {
+func loadInt(name string, out *reflect.Value) {
 	data, found := syscall.Getenv(name)
 	if !found {
 		return
 	}
+
 	d, err := strconv.ParseInt(data, 10, 64)
 	if err != nil {
 		panic(fmt.Errorf("field type of %s cannot be parsed into integer", name))
 	}
 
 	out.SetInt(d)
+}
+
+func loadUint(name string, out *reflect.Value) {
+	data, found := syscall.Getenv(name)
+	if !found {
+		return
+	}
+
+	d, err := strconv.ParseUint(data, 10, 64)
+	if err != nil {
+	}
+
+	out.SetUint(d)
 }
 
 func loadBool(name string, out *reflect.Value) {
