5a1b30f0d6ce5e6b4de0e723b28ff9f0c74e934e
賈仁宇 <raymondchia@rayark.com>
2020-08-12 10:15:10 +0800

    chore: add comment - add comment for `CustomHandleEnvVarsOption`

--- option.go
+++ option.go
@@ -2,6 +2,11 @@ package envconf
 
 type Option func(*loader)
 
+// CustomHandleEnvVarsOption creates an option that calls `cb` with a map
+// indicating the environment variables checked by envconf.
+//
+// The map keys are the environment variable names that are checked by envconf,
+// and the values present whether the corresponding environment variables are set.
 func CustomHandleEnvVarsOption(cb func(map[string]bool)) Option {
 	return func(l *loader) {
 		l.handleEnvironmentVariables = cb
